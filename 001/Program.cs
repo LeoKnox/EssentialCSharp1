75d6101e6605ed3d95d5c43ec4d757331f549c15
LeoKnox <LeoKnox@users.noreply.github.com>
2020-08-22 13:09:14 -0700

    Add files via upload

--- 001/Program.cs
+++ 001/Program.cs
@@ -9,9 +9,15 @@ namespace Essential01
         static void Main(string[] args)
         {
             aNumber = 5;
+            int bigInt = int.MaxValue; // MaxVlue can be used to show max value of int type
+            int smInt = int.MinValue;
+            uint posInt = uint.MaxValue;
+            float pie = 3.14f; // f required to make it float over double.
+            bool amI = false; // bool is boolean and true/false lowercase
             var message = string.Format($"This is {title} and a number {aNumber}");
+            message = "'long' and 'short' are also int values";
+            message = ($"this is big int {bigInt}, smInt {smInt} and posInt {posInt}");
             Console.WriteLine(message);
-            Console.ReadLine();
         }
     }
 }
