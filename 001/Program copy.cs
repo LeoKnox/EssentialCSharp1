ba397fe3930b225082a2f6e3fcde890cf95e8ced
LeoKnox <LeoKnox@users.noreply.github.com>
2020-08-22 14:56:55 -0700

    Add files via upload

---
+++ 001/Program copy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Essential011
+{
+    class P2rogram
+    {
+    public static string title = "My programs";
+    public static int aNumber;
+        static void Main2(string[] args)
+        {
+            aNumber = 5;
+            int bigInt = int.MaxValue; // MaxVlue can be used to show max value of int type
+            int smInt = int.MinValue;
+            uint posInt = uint.MaxValue;
+            float pie = 3.14f; // f required to make it float over double.
+            bool amI = false; // bool is boolean and true/false lowercase
+            var message = string.Format($"This is {title} and a number {aNumber}");
+            message = "'long' and 'short' are also int values";
+            message = ($"this is big int {bigInt}, smInt {smInt} and posInt {posInt}");
+            Console.WriteLine(message);
+            Console.WriteLine(int.Parse("15"));
+            string test = "12345";
+            test = test.PadLeft(9, '0');
+            Console.WriteLine(test);
+        }
+    }
+}
