cd4769376a0ddccc75bb86116613ccc91e672c0f
LeoKnox <LeoKnox@users.noreply.github.com>
2020-08-22 15:13:48 -0700

    Add files via upload

--- 001/Program.cs
+++ 001/Program.cs
@@ -8,20 +8,16 @@ namespace Essential01
     public static int aNumber;
         static void Main(string[] args)
         {
-            aNumber = 5;
-            int bigInt = int.MaxValue; // MaxVlue can be used to show max value of int type
-            int smInt = int.MinValue;
-            uint posInt = uint.MaxValue;
-            float pie = 3.14f; // f required to make it float over double.
-            bool amI = false; // bool is boolean and true/false lowercase
-            var message = string.Format($"This is {title} and a number {aNumber}");
-            message = "'long' and 'short' are also int values";
-            message = ($"this is big int {bigInt}, smInt {smInt} and posInt {posInt}");
-            Console.WriteLine(message);
-            Console.WriteLine(int.Parse("15"));
-            string test = "12345";
-            test = test.PadLeft(9, '0');
-            Console.WriteLine(test);
+            // methods
+            string testString = " abcDEF    ";
+            Console.WriteLine(testString.Trim());
+            Console.WriteLine(testString.ToUpper().TrimEnd());
+            string longString = "This is a really long string, and more to come.";
+            Console.WriteLine(longString.Substring(5, 3));
+            // properties
+            Console.WriteLine(longString.Length);
+            string challenge = "   Text processing in C# is really great!    ";
+            Console.WriteLine(challenge.Trim().Substring(8, challenge.Length-17).ToUpper());
         }
     }
 }
