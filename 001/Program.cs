fa3e04cdfc6073e4edeb02552ad4aba715f44966
LeoKnox <LeoKnox@users.noreply.github.com>
2020-08-21 19:08:37 -0700

    Create Program.cs

---
+++ 001/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Essential01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start programming here");
+            Console.WriteLine("Press enter to continue");
+            Console.ReadLine();
+        }
+    }
+}
