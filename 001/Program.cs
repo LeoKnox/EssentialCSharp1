f451001a096ff63319a7a4505d284455c216123b
LeoKnox <LeoKnox@users.noreply.github.com>
2020-08-21 19:27:14 -0700

    Add files via upload

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,13 +1,16 @@
-using System;
+﻿using System;
 
 namespace Essential01
 {
     class Program
     {
+    public static string title = "My programs";
+    public static int aNumber;
         static void Main(string[] args)
         {
-            Console.WriteLine("Start programming here");
-            Console.WriteLine("Press enter to continue");
+            aNumber = 5;
+            var message = string.Format($"This is {title} and a number {aNumber}");
+            Console.WriteLine(message);
             Console.ReadLine();
         }
     }
