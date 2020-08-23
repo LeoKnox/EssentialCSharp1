39d91d2c62a2fa8a6701c23783125efd7b79e43b
LeoKnox <LeoKnox@users.noreply.github.com>
2020-08-23 16:10:31 -0700

    Add files via upload

--- 001/Program.cs
+++ 001/Program.cs
@@ -11,6 +11,13 @@ namespace Essential01
         {
             StringBuilder sb = new StringBuilder();
             sb.Append("First Line appended");
+            sb.Append("Note to self append is not spelled apppend.");
+            StringBuilder sb2 = new StringBuilder("With text!");
+            sb2.AppendLine("Added new line."); // results in same line
+            sb2 = new StringBuilder();
+            sb2.AppendLine("Time for a break.");
+            sb2.AppendLine("Added new line.");
+            Console.WriteLine(sb2.ToString());
         }
     }
 }
---
+++ 001/text1.cs
@@ -0,0 +1,12 @@
+/*
+            // methods
+            string testString = " abcDEF    ";
+            Console.WriteLine(testString.Trim());
+            Console.WriteLine(testString.ToUpper().TrimEnd());
+            string longString = "This is a really long string, and more to come.";
+            Console.WriteLine(longString.Substring(5, 3));
+            // properties
+            Console.WriteLine(longString.Length);
+            string challenge = "   Text processing in C# is really great!    ";
+            Console.WriteLine(challenge.Trim().Substring(8, challenge.Trim().Length-17).ToUpper());
+            */
