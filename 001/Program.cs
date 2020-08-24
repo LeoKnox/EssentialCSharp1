ee6cf9192ee07f47f1175ff6f71ae0aec7a27865
LeoKnox <LeoKnox@users.noreply.github.com>
2020-08-23 23:36:38 -0700

    Add files via upload

--- 001/Program.cs
+++ 001/Program.cs
@@ -10,6 +10,11 @@ namespace Essential01
             string tex = "Word One";
             float somNum = 10100.33f;
             var current = DateTime.Now;
+            string xx = "15,329";
+            int result;
+            int.TryParse(xx, out result); // returns false, if no comma would return true and set reult = to number always returns bool
+            int zz = int.Parse(xx.Replace(",", ""));
+            Console.WriteLine(zz);
             Console.WriteLine(string.Format(" This is {0} at {1:0,0.000} and is {2:t}", tex,somNum,current));
         }
     }
