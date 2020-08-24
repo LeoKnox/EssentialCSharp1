1995af0376829be26dd6f82ea292ebc1db0eb251
LeoKnox <LeoKnox@users.noreply.github.com>
2020-08-23 22:58:45 -0700

    Add files via upload

--- 001/Program.cs
+++ 001/Program.cs
@@ -5,19 +5,12 @@ namespace Essential01
 {
     class Program
     {
-    public static string title = "My programs";
-    public static int aNumber;
         static void Main(string[] args)
         {
-            StringBuilder sb = new StringBuilder();
-            sb.Append("First Line appended");
-            sb.Append("Note to self append is not spelled apppend.");
-            StringBuilder sb2 = new StringBuilder("With text!");
-            sb2.AppendLine("Added new line."); // results in same line
-            sb2 = new StringBuilder();
-            sb2.AppendLine("Time for a break.");
-            sb2.AppendLine("Added new line.");
-            Console.WriteLine(sb2.ToString());
+            string tex = "Word One";
+            float somNum = 101.33f;
+            DateTime current = DateTime.Now;
+            Console.WriteLine(string.Format(" This is {0} with {1} and is {2}", tex,somNum,current));
         }
     }
 }
