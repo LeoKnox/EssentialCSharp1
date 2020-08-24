0b1e57677716e0316ca65308c39ea610118e0f4a
LeoKnox <LeoKnox@users.noreply.github.com>
2020-08-23 23:15:56 -0700

    Add files via upload

--- 001/Program.cs
+++ 001/Program.cs
@@ -8,9 +8,9 @@ namespace Essential01
         static void Main(string[] args)
         {
             string tex = "Word One";
-            float somNum = 101.33f;
-            DateTime current = DateTime.Now;
-            Console.WriteLine(string.Format(" This is {0} with {1} and is {2}", tex,somNum,current));
+            float somNum = 10100.33f;
+            var current = DateTime.Now;
+            Console.WriteLine(string.Format(" This is {0} at {1:0,0.000} and is {2:t}", tex,somNum,current));
         }
     }
 }
