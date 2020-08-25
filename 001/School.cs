399009885a2d00a85584d5edc9652a9cf0272448
LeoKnox <LeoKnox@users.noreply.github.com>
2020-08-24 18:51:06 -0700

    Add files via upload

---
+++ 001/School.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Essential01
+{
+    class School
+    {
+        string SchoolName;
+        string SchoolCity;
+        string SchoolMascot;
+        string SchoolColor;
+    }
+}
