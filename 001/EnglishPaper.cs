82c49d4aeedff57cbddf0b823096607aca8ee156
LeoKnox <LeoKnox@users.noreply.github.com>
2020-08-29 21:12:21 -0700

    Add files via upload

---
+++ 001/EnglishPaper.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Essential01
+{
+    public class EnglishPaper
+    {
+        public string Title { get; set; }
+        public int MinimumWordCount { get; set; }
+        public string PaperText { get; set; }
+    }
+}
---
+++ 001/ScienceExperiment.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Essential01
+{
+    public class ScienceExperiment
+    {
+        public string Hypothesis { get; set; }
+        public string Materials { get; set; }
+        public string Method { get; set; }
+        public string Conclusion { get; set; }
+    }
+}
